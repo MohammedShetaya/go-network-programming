bf3d6e573e0e73c27d37516a80e5923d7027c444
MohammedShetaya <mohamed.shetia@careem.com>
2024-06-10 11:08:15 +0300

    add tcp listener

--- TCP_test.go
+++ TCP_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestLister(t *testing.T) {
+func TestListener(t *testing.T) {
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		t.Fatal(err)
@@ -14,4 +14,18 @@ func TestLister(t *testing.T) {
 		_ = listener.Close()
 	}()
 	t.Logf("listening on %s", listener.Addr().String())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		go func(c net.Conn) {
+			defer func() {
+				_ = c.Close()
+			}()
+			t.Logf("Accepted connection from %s", c.RemoteAddr().String())
+		}(conn)
+	}
 }
